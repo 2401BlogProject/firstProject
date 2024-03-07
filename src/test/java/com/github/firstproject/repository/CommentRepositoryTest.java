35f6997bd2408fb42dded2f66251e2e85f99c309
qnelldo <131656049+qnelldo@users.noreply.github.com>
2024-03-07 19:25:51 +0900

    POST 테스트 후 반영

--- src/test/java/com/github/firstproject/repository/CommentRepositoryTest.java
+++ src/test/java/com/github/firstproject/repository/CommentRepositoryTest.java
@@ -1,5 +1,6 @@
 package com.github.firstproject.repository;
 
+import com.github.firstproject.repository.comment.CommentRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
